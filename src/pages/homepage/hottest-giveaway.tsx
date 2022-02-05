f8ff31cb73aef338f2777704e3a86e2bb660a16a
Steve Yu <steveyuowo@gmail.com>
2022-02-05 18:02:33 +0800

    Feat: Api data for landing-page

--- src/pages/homepage/hottest-giveaway.tsx
+++ src/pages/homepage/hottest-giveaway.tsx
@@ -1,8 +1,15 @@
+import dayjs from "dayjs";
 import React from "react";
 import FireFill from "~assets/fire-fill.svg";
+import { usePrizePoolDisplayLists } from "~state/prize";
 import GiveawayCard from "./giveaway-card";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+dayjs.extend(advancedFormat);
 
 function HottestGiveaway() {
+  const prizes = usePrizePoolDisplayLists();
+  if (!prizes) return null;
+
   return (
     <section
       id="hottest-giveaway"
@@ -22,12 +29,56 @@ function HottestGiveaway() {
         className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-12 home-page-giveaway-cards"
         style={{ marginTop: "70px" }}
       >
-        <GiveawayCard />
-        <GiveawayCard />
-        <GiveawayCard />
-        <GiveawayCard />
-        <GiveawayCard />
-        <GiveawayCard />
+        {prizes
+          .filter((prize) => prize.status === "ONGOING")
+          .map(({ cover, name, id, prize_type, requirements, end_time }) => (
+            <GiveawayCard
+              image={cover}
+              title={name}
+              prizeType={
+                {
+                  NFT: "NFT",
+                  Crypto: "Crypto",
+                  NFT_Crypto: "NFT and crypto",
+                  No_Prize: "No prize",
+                }[prize_type]
+              }
+              requirements={((requirements) => {
+                if (!requirements) return "No requirements";
+                let follow, retweet, like;
+                try {
+                  JSON.parse(requirements).forEach(
+                    (requirement: {
+                      requirement_type:
+                        | "twitter_follow"
+                        | "twitter_retweet"
+                        | "twitter_like";
+                    }) => {
+                      if (requirement?.requirement_type === "twitter_follow")
+                        follow = true;
+                      if (requirement?.requirement_type === "twitter_retweet")
+                        retweet = true;
+                      if (requirement?.requirement_type === "twitter_like")
+                        like = true;
+                    }
+                  );
+                  if (follow && retweet && like)
+                    return "Follow, like and retweet";
+                  if (follow && like) return "Follow and like";
+                  if (retweet && like) return "Like and retweet";
+                  if (follow && retweet) return "Follow and retweet";
+                  if (follow) return "Follow";
+                  if (like) return "Like";
+                  if (retweet) return "Retweet";
+                  return "No requirements";
+                } catch (e) {
+                  return "No requirements";
+                }
+              })(requirements)}
+              endTime={dayjs(end_time).format("Do MMM. HH:mm")}
+              detailLink={`/box/${id}`}
+            />
+          ))}
       </div>
     </section>
   );
