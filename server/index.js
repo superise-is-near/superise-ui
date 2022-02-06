5536d6b6862ae52d599af8cf281b04d5801d5e24
0000ff <kidliaa+hash0000ff@gmail.com>
2022-02-06 15:18:10 +0800

    Serve static index.html for the React App

--- server/index.js
+++ server/index.js
@@ -227,5 +227,9 @@ async function main() {
   });
 
   app.use(express.static(path.resolve(__dirname, "../dist")));
+  app.use("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../dist/index.html"));
+  });
+
   if (!process.env.NODE_ENV) app.use(proxy);
 }
