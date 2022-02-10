cab4068f71899f5d3970065846b7f0063f212394
Steve Yu <steveyuowo@gmail.com>
2022-02-10 23:52:46 +0800

    Feat: Add padding in both sides

--- src/pages/create-box/create-box.tsx
+++ src/pages/create-box/create-box.tsx
@@ -106,68 +106,70 @@ const CreateBox: FC = () => {
   );
 
   return (
-    <main className="m-auto lg:max-w-2xl">
-      <h1 className="text-5xl font-bold">Create Giveaway</h1>
-      {/* progress 1, select prize */}
-      <SecondaryTitle
-        select={progress >= 0}
-        className="mt-12"
-        icon={Clothes}
-        deepIcon={DeepClothes}
-      >
-        ADD GIVEAWAY PRIZES
-      </SecondaryTitle>
-      <GiveAwayPrizes
-        collapsed={progress !== 0}
-        setProgress={setProgress}
-        parasNfts={parasNfts}
-        cryptos={cryptos}
-        setParasNfts={setParasNfts}
-        setCryptos={setCryptos}
-      />
-
-      {/* progress 2, select requirements */}
-      <SecondaryTitle
-        select={progress >= 1}
-        className="mt-12"
-        icon={Clock}
-        deepIcon={DeepClock}
-      >
-        Requirements &amp; Timing
-      </SecondaryTitle>
-      {(sessionStorage.getItem("like") ||
-        sessionStorage.getItem("retweet") ||
-        sessionStorage.getItem("follow") ||
-        progress >= 1) && (
-        <RequirementsTiming
-          collapsed={progress !== 1}
+    <div className="max-w-2xl px-5 m-auto mb-6 lg:max-w-7xl">
+      <main className="m-auto lg:max-w-2xl">
+        <h1 className="text-5xl font-bold">Create Giveaway</h1>
+        {/* progress 1, select prize */}
+        <SecondaryTitle
+          select={progress >= 0}
+          className="mt-12"
+          icon={Clothes}
+          deepIcon={DeepClothes}
+        >
+          ADD GIVEAWAY PRIZES
+        </SecondaryTitle>
+        <GiveAwayPrizes
+          collapsed={progress !== 0}
           setProgress={setProgress}
+          parasNfts={parasNfts}
+          cryptos={cryptos}
+          setParasNfts={setParasNfts}
+          setCryptos={setCryptos}
+        />
+
+        {/* progress 2, select requirements */}
+        <SecondaryTitle
+          select={progress >= 1}
+          className="mt-12"
+          icon={Clock}
+          deepIcon={DeepClock}
+        >
+          Requirements &amp; Timing
+        </SecondaryTitle>
+        {(sessionStorage.getItem("like") ||
+          sessionStorage.getItem("retweet") ||
+          sessionStorage.getItem("follow") ||
+          progress >= 1) && (
+          <RequirementsTiming
+            collapsed={progress !== 1}
+            setProgress={setProgress}
+            progress={progress}
+            follow={follow}
+            setFollow={setFollow}
+            retweet={retweet}
+            setRetweet={setRetweet}
+            like={like}
+            setLike={setLike}
+          />
+        )}
+
+        {/* progress 3, send tweet */}
+        <SecondaryTitle
+          select={progress === 2}
+          className="mt-12"
+          icon={Hor}
+          deepIcon={DeepHor}
+        >
+          CUSTOMIZE TWEET
+        </SecondaryTitle>
+        <CustomTweet
           progress={progress}
           follow={follow}
-          setFollow={setFollow}
-          retweet={retweet}
-          setRetweet={setRetweet}
           like={like}
-          setLike={setLike}
+          retweet={retweet}
         />
-      )}
-
-      {/* progress 3, send tweet */}
-      <SecondaryTitle
-        select={progress === 2}
-        className="mt-12"
-        icon={Hor}
-        deepIcon={DeepHor}
-      >
-        CUSTOMIZE TWEET
-      </SecondaryTitle>
-      <CustomTweet
-        progress={progress}
-        follow={follow}
-        like={like}
-        retweet={retweet}
-      />
-    </main>
+      </main>
+    </div>
   );
 };
 
