4feaf2555f02ba47e6f157a2b5f8b4b10e446784
Steve Yu <steveyuowo@gmail.com>
2022-02-19 12:14:30 +0800

    feat: Add github link in footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -17,11 +17,6 @@ export default () => {
         </div>
       </Link>
       <div className="flex items-end">
-        <a href="mailto:contact@superise.xyz" target="_blank">
-          <div className="ml-4 font-bold cursor-pointer hover:underline">
-            CONTACT US
-          </div>
-        </a>
         <a href="https://twitter.com/superise_" target="_blank">
           <div className="ml-4 font-bold cursor-pointer hover:underline">
             TWITTER
@@ -31,6 +26,18 @@ export default () => {
         <a href="https://discord.gg/pPDXrKV6" target="_blank">
           <div className="ml-4 font-bold cursor-pointer hover:underline">
             DISCORD
+          </div>
+        </a>
+
+        <a href="https://github.com/surprise-is-near" target="_blank">
+          <div className="ml-4 font-bold cursor-pointer hover:underline">
+            GITHUB
+          </div>
+        </a>
+
+        <a href="mailto:contact@superise.xyz" target="_blank">
+          <div className="ml-4 font-bold cursor-pointer hover:underline">
+            CONTACT US
           </div>
         </a>
       </div>
