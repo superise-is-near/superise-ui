5c877a9a17ab91b594e86e0ae829bbcebf8c77eb
Steve Yu <steveyuowo@gmail.com>
2022-02-19 12:39:45 +0800

    feat: add link in footer & home-hero

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -18,19 +18,19 @@ export default () => {
       </Link>
       <div className="flex flex-end">
         <a href="https://twitter.com/superise_" target="_blank">
-          <div className="ml-4 text-sm sm:text-base font-bold cursor-pointer hover:underline">
+          <div className="ml-4 text-sm sm:text-base font-semibold cursor-pointer hover:underline">
             TWITTER
           </div>
         </a>
 
         <a href="https://discord.gg/pPDXrKV6" target="_blank">
-          <div className="ml-4 text-sm sm:text-base font-bold cursor-pointer hover:underline">
+          <div className="ml-4 text-sm sm:text-base font-semibold cursor-pointer hover:underline">
             DISCORD
           </div>
         </a>
 
         <a href="mailto:contact@superise.xyz" target="_blank">
-          <div className="ml-4 text-sm sm:text-base font-bold cursor-pointer hover:underline">
+          <div className="ml-4 text-sm sm:text-base font-semibold cursor-pointer hover:underline">
             CONTACT US
           </div>
         </a>
--- src/pages/homepage/home-hero.tsx
+++ src/pages/homepage/home-hero.tsx
@@ -14,31 +14,36 @@ function HomeHero() {
         <PrimaryButton className="px-8 py-4 mt-8">Get Started</PrimaryButton>
       </a>
       <img src={heroImage} className="pt-4 mt-12" />
+
       <div className="flex mt-8">
-        <div className="flex flex-col items-center mr-10 text-sm font-semibold text-center text-gray-700">
-          <div className="w-6 h-6 mb-2 overflow-hidden">
-            <img
-              className="w-6 h-6"
-              width="24px"
-              height="24px"
-              src={BookIcon}
-              alt="book icon"
-            />
+        <a href="https://github.com/surprise-is-near" target="_blank">
+          <div className="flex flex-col items-center mr-10 text-sm font-semibold text-center text-gray-700">
+            <div className="w-6 h-6 mb-2 overflow-hidden">
+              <img
+                className="w-6 h-6"
+                width="24px"
+                height="24px"
+                src={BookIcon}
+                alt="book icon"
+              />
+            </div>
+            <div className="font-bold">Proudly Opensourced</div>
           </div>
-          <div>Proudly Opensourced</div>
-        </div>
-        <div className="flex flex-col items-center text-sm font-semibold text-center text-gray-700">
-          <div className="w-6 h-6 mb-2 overflow-hidden">
-            <img
-              className="w-6 h-6"
-              width="24px"
-              height="24px"
-              src={NearIcon}
-              alt="near icon"
-            />
+        </a>
+        <a href="https://near.org/" target="_blank">
+          <div className="flex flex-col items-center text-sm font-semibold text-center text-gray-700">
+            <div className="w-6 h-6 mb-2 overflow-hidden">
+              <img
+                className="w-6 h-6"
+                width="24px"
+                height="24px"
+                src={NearIcon}
+                alt="near icon"
+              />
+            </div>
+            <div className="font-bold">Powered by NEAR</div>
           </div>
-          <div>Powered by NEAR</div>
-        </div>
+        </a>
       </div>
     </section>
   );
