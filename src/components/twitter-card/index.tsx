d415eea21cde785671ad649c5d2454f351ddc967
0000ff <kidliaa+hash0000ff@gmail.com>
2022-02-21 09:36:53 +0800

    Fix tweet load unstable issue

--- src/components/twitter-card/index.tsx
+++ src/components/twitter-card/index.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const TwitterCard = ({ url }: { url: string }) => {
+  useEffect(() => {
+    if (typeof twttr !== "undefined") {
+      twttr.widgets.load(document.getElementById("twitter-timeline"));
+    }
+  }, []);
   return (
     <div className="overflow-y-scroll h-72">
-      <div className="">
-        <blockquote className="twitter-tweet">
-          <a href={url}></a>
-        </blockquote>
-      </div>
+      <blockquote className="twitter-tweet">
+        <a href={url} className="twitter-timeline" />
+      </blockquote>
     </div>
   );
 };
