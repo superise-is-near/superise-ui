779d0734515ca4002c59e11921cf0be5d0351d3a
Steve Yu <steveyuowo@gmail.com>
2022-02-22 09:38:01 +0800

    feat: support seo & og protocol

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import SiteHeader from "~components/layout/SiteHeader";
 import Footer from "~components/layout/footer";
 import CreateBox from "~pages/create-box";
 import HomePage from "~pages/homepage";
+import { Helmet } from "react-helmet";
 
 Modal.defaultStyles = {
   overlay: {
@@ -37,6 +38,47 @@ Modal.setAppElement("#root");
 function App() {
   return (
     <Router>
+      <Helmet>
+        <title>Superise</title>
+        <link rel="canonical" href={window.location.href} />
+        <meta property="og:url" content={window.location.href} />
+        <meta
+          property="og:title"
+          content="Superise: Crypto &amp; NFT giveaways like it's 2077. "
+        />
+        <meta
+          property="og:description"
+          content="Offering crypto tools to make building and launching projects easier."
+        />
+        <meta
+          property="og:image"
+          content="https://testnet.superise.xyz/hero-image.4acbe8a5.png"
+        />
+        <meta
+          name="twitter:card"
+          content="Superise: Crypto &amp; NFT giveaways like it's 2077. "
+        />
+        <meta
+          name="twitter:title"
+          content="Superise: Crypto &amp; NFT giveaways like it's 2077. "
+        />
+        <meta
+          name="twitter:description"
+          content="Offering crypto tools to make building and launching projects easier."
+        />
+        <meta
+          name="twitter:image"
+          content="https://testnet.superise.xyz/hero-image.4acbe8a5.png"
+        />
+        <meta
+          name="description"
+          content="Crypto &amp; NFT giveaways like it's 2077. Offering crypto tools to make building and launching projects easier."
+        />
+        <meta
+          name="keywords"
+          content="crypto, marketing tools, superise, giveaway tools"
+        />
+      </Helmet>
       <div className="max-w-2xl px-5 m-auto mb-6 lg:max-w-7xl">
         <SiteHeader />
       </div>
