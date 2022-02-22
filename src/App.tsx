683a9e7f668fbca2f490946481412e186d03a692
Steve Yu <steveyuowo@gmail.com>
2022-02-22 10:13:02 +0800

    fix: move static seo option to html

--- src/App.tsx
+++ src/App.tsx
@@ -39,45 +39,8 @@ function App() {
   return (
     <Router>
       <Helmet>
-        <title>Superise</title>
         <link rel="canonical" href={window.location.href} />
         <meta property="og:url" content={window.location.href} />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          content="Superise: Crypto &amp; NFT giveaways like it's 2077. "
-        />
-        <meta
-          property="og:description"
-          content="Offering crypto tools to make building and launching projects easier."
-        />
-        <meta
-          property="og:image"
-          content="https://testnet.superise.xyz/hero-image.4acbe8a5.png"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@superise" />
-        <meta name="twitter:creator" content="@superise_team" />
-        <meta
-          name="twitter:title"
-          content="Superise: Crypto &amp; NFT giveaways like it's 2077. "
-        />
-        <meta
-          name="twitter:description"
-          content="Offering crypto tools to make building and launching projects easier."
-        />
-        <meta
-          name="twitter:image"
-          content="https://testnet.superise.xyz/hero-image.4acbe8a5.png"
-        />
-        <meta
-          name="description"
-          content="Crypto &amp; NFT giveaways like it's 2077. Offering crypto tools to make building and launching projects easier."
-        />
-        <meta
-          name="keywords"
-          content="crypto, marketing tools, superise, giveaway tools"
-        />
       </Helmet>
       <div className="max-w-2xl px-5 m-auto mb-6 lg:max-w-7xl">
         <SiteHeader />
